9386fdf446f7e1a9ed50f05499a4cf589f930926
mehdibh <mehdibhaddou@gmail.com>
2023-05-07 23:59:42 +0200

    Plug API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
+import axios from "axios";
 import './App.css'
 import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
+    const [request, setRequest] = useState("/");
 
   return (
     <>
@@ -12,7 +14,23 @@ function App() {
         <button className={"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"} onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <button className={"bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"} onClick={
+            async () => {
+                    try {
+                        const response = await axios.get('https://tgy7h9fntd.execute-api.eu-west-3.amazonaws.com/dev/api/avatars');
+                        setRequest(JSON.stringify(response.data))
+                    } catch (error) {
+                        console.log(error);
+                    }
+                }
+            }
+        >
+            click to tick api
+        </button>
       </div>
+        <div>
+            {request}
+        </div>
     </>
   )
 }
